03af891c3211c1e2c9d3f0aa4f598fa7001cc4ff
DipaliDhonnar <drdhonnar@gmail.com>
2024-12-02 23:28:08 +0530

    chnages add

--- js/festivegifting.js
+++ js/festivegifting.js
@@ -1,14 +1,16 @@
 function increment(counterId) {
-    var counter = document.getElementById(counterId);
-    var currentValue = parseInt(counter.innerText);
-    counter.innerText = ++ currentValue;
+  var counter = document.getElementById(counterId);
+  var currentValue = parseInt(counter.innerText);
+  counter.innerText = ++ currentValue;
+  alert("one item added successfully" );
+}
+
+function decrement(counterId) {
+  var counter = document.getElementById(counterId);
+  var currentValue = parseInt(counter.innerText);
+  if (currentValue>0) {
+    counter.innerText = --currentValue;
+    alert("one item remove successfully" );
   }
-  
-  function decrement(counterId) {
-    var counter = document.getElementById(counterId);
-    var currentValue = parseInt(counter.innerText);
-    if (currentValue>0) {
-      counter.innerText = --currentValue;
-    }
-  
-  }
+
+}
