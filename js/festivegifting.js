1395755b109aa44b62e86da1e1d42bd545499169
DipaliDhonnar <drdhonnar@gmail.com>
2024-12-03 15:58:05 +0530

    search add

--- js/festivegifting.js
+++ js/festivegifting.js
@@ -13,3 +13,18 @@ function decrement(counterId) {
   }
 
 }
+
+function openModel() {
+  const overlayElement = document.getElementById('overlay');
+  overlayElement.style.display = 'flex';
+}
+
+function closeModel() {
+  const overlayElement = document.getElementById('overlay');
+  overlayElement.style.display = 'none';
+}
+
+function okButton() {
+  const overlayElement = document.getElementById('overlay');
+  overlayElement.style.visibility = 'none';
+}
